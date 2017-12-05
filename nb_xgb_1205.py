1d50c61ea9c596e2b7818e0f4168afcdc24014ea
Lingrui <lingruicai@gmail.com>
2017-12-05 15:46:35 -0500

    update

--- nb_xgb_1205.py
+++ nb_xgb_1205.py
@@ -19,6 +19,18 @@ from sklearn.feature_extraction.text import TfidfVectorizer, CountVectorizer
 import nltk
 from nltk.corpus import stopwords
 
+param = {'objective': 'binary:logistic',
+        'eta': 0.001,
+        'max_depth': 3, 
+        'silent': 1, 
+        'eval_metric': 'auc' 
+        'min_child_weight': 'child',
+        'subsample' 0.5, 
+        'colsample_bytree': 'colsample',
+        'seed': 2017
+}
+
+num_rounds = 1000
 
 def main():
     #Set seed for reproducibility
@@ -37,7 +49,28 @@ def load_data(file_name,column_name):
         load_df = pd.concat([load_df,pred_df],axis=0)
     return load_df
 
-def metaFeature(df):
-    ## Number of words in the text ##
+def metaFeature(d):
+    # Number of words in the text ##
+    d["num_words"] = d["text"].apply(lambda x: len(str(x).split()))
+    # Number of unique words in the text ##
+    d["num_unique_words"] = d["text"].apply(lambda x: len(set(str(x).split())))
+    # Number of characters in the text ##
+    d["num_chars"] = d["text"].apply(lambda x: len(str(x)))
+    # Number of stopwords in the text ##
+    d["num_stopwords"] = d["text"].apply(lambda x: len([w for w in str(x).lower().split() if w in eng_stopwords]))
+    # Number of punctuations in the text ##
+    d["num_punctuations"] = d['text'].apply(lambda x: len([c for c in str(x) if c in string.punctuation]) )
+    # Number of title case words in the text ##
+    d["num_words_upper"] = d["text"].apply(lambda x: len([w for w in str(x).split() if w.isupper()]))
+    ## Number of title case words in the text ##
+    d["num_words_title"] = d["text"].apply(lambda x: len([w for w in str(x).split() if w.istitle()]))
+    ## Average length of the words in the text ##
+    d["mean_word_len"] = d["text"].apply(lambda x: np.mean([len(w) for w in str(x).split()]))
+
+
+
+
+
+
 if __name__ == '__main__':
     main()
