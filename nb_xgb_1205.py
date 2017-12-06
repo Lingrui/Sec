89493828da99e17d7ddc9b3e646d4e9d6a2e0bff
Lingrui <lingruicai@gmail.com>
2017-12-06 11:05:40 -0500

    add SVD

--- nb_xgb_1205.py
+++ nb_xgb_1205.py
@@ -45,11 +45,18 @@ def main():
     #Load the data from the CSV files
     training_data = load_data(train_file,train_col)
     test_data = load_data(test_file,test_col)
-    print ("Vectorize data...")
+    print ("TFIDF Vectorize data...")
     os.system('date')
-    x,y,z = tfidf_v(training_data,test_data,'char')
+
+    x,y,z = TfidfV(training_data,test_data,'char')
     print (x.shape)
 
+    print ('Count Vectorize data...')
+    
+    os.system('date')
+    os.system('date')
+    os.system('date')
+    os.system('date')
 
 def load_data(file_name,column_name):
     load_df = pd.DataFrame(columns=column_name)
@@ -76,14 +83,39 @@ def metaFeature(d):
     d["mean_word_len"] = d["text"].apply(lambda x: np.mean([len(w) for w in str(x).split()]))
 
 ### Fit transform the tfidf vectorizer ###
-def tfidf_v(train_df,test_df,split_type):
+def TfidfV(train_df,test_df,split_type):
     tfidf_vec = TfidfVectorizer(stop_words='english', ngram_range=(1,1),analyzer=split_type)
     full_tfidf = tfidf_vec.fit_transform(train_df['text'].values.tolist() + test_df['text'].values.tolist())
     train_tfidf = tfidf_vec.transform(train_df['text'].values.tolist())
     test_tfidf = tfidf_vec.transform(test_df['text'].values.tolist())
     return full_tfidf, train_tfidf, test_tfidf 
 
+### Fit transform the count vectorizer ###
+def CountV(train_df,test_df,split_type):
+    count_vec = CountVectorizer(stop_words='english', ngram_range=(1,1),analyzer=split_type)
+    count_vec.fit(train_df['text'].values.tolist() + test_df['text'].values.tolist())
+    train_count = count_vec.transform(train_df['text'].values.tolist())
+    test_count = count_vec.transform(test_df['text'].values.tolist())
+    return train_count,test_count
 
+### SVD on TFIDF 
+def SVD(train_df,test_df,split_type,full_tfidf,train_tfidf,test_tfidf):
+    n_comp = 20  #value for LSA
+    svd_obj = TruncatedSVD(n_components=n_comp, algorithm='arpack')
+    svd_obj.fit(full_tfidf)
+    train_svd = pd.DataFrame(svd_obj.transform(train_tfidf))
+    test_svd = pd.DataFrame(svd_obj.transform(test_tfidf))
+        
+    train_svd.columns = ['svd_'+split_type+'_'+str(i) for i in range(n_comp)]
+    test_svd.columns = ['svd_'+split_type+'_'+str(i) for i in range(n_comp)]
+    train_df = pd.concat([train_df, train_svd], axis=1)
+    test_df = pd.concat([test_df, test_svd], axis=1)
+    return train_df,test_df
+
+def runMNB(train_X,train_y,test_X):
+    model = naive_bayes.MultinomialNB()
+    model.fit(train_X,train_y)
+    pred_test_y = model.predict_proba(test_X)
 
 
 
