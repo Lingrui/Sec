8278152b5c6999c081bf76e1144f38fb22b2a254
Lingrui <lingruicai@gmail.com>
2017-12-15 13:37:49 -0500

    map to dict

--- Dec14_tfidf_Test/read_npy_sort.py
+++ Dec14_tfidf_Test/read_npy_sort.py
@@ -4,20 +4,18 @@ from __future__ import division
 from __future__ import absolute_import
 import pickle
 import numpy as np
-DICT = []
+DICT = {}  
 i = 0 
 with open('test.dictionary.txt','r') as dic:
     for l in dic:
         word = l.strip()
-        DICT.append((i,word))
+        DICT[i] = word
         i += 1 
-print (DICT.get('0')) 
+#print (DICT[1139]) 
 c = np.load("test_SVD.npy")
-'''
 for line in c:
     #print(line.argsort()[-10:][::-1])
     for s in line.argsort()[-3:][::-1]:
 
-        print(s,line[s])
+        print(DICT[s],'\t',line[s])
     print ()
-'''
