a2cdd91858000f28c0410ca5dd3329607862dc71
Lingrui <lingruicai@gmail.com>
2017-12-14 23:58:04 -0500

    print matrix

--- Dec14_tfidf_Test/read_npy_sort.py
+++ Dec14_tfidf_Test/read_npy_sort.py
@@ -1,1 +1,15 @@
+#!/usr/bin/env python
+from __future__ import print_function
+from __future__ import division
+from __future__ import absolute_import
+import pickle
+import numpy as np
 c = np.load("test_SVD.npy")
+for line in c:
+    '''
+    line.sort()
+    print(line)
+    '''
+    print(line.argsort()[-10:][::-1])
+#f = open ("Top10_words.txt","w+")
+#    print(line.argsort()[-10:][::-1],file =f )
