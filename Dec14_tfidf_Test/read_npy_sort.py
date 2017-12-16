445eb844c4009854b898b5168c2e7b23bae0ffdd
Lingrui <lingruicai@gmail.com>
2017-12-16 10:23:24 -0500

    add fig and html

--- Dec14_tfidf_Test/read_npy_sort.py
+++ Dec14_tfidf_Test/read_npy_sort.py
@@ -4,6 +4,11 @@ from __future__ import division
 from __future__ import absolute_import
 import pickle
 import numpy as np
+from scipy.misc import imread
+import matplotlib.pyplot as plt
+
+from wordcloud import WordCloud, ImageColorGenerator
+
 DICT = {}  
 i = 0 
 with open('test.dictionary.txt','r') as dic:
@@ -13,9 +18,31 @@ with open('test.dictionary.txt','r') as dic:
         i += 1 
 #print (DICT[1139]) 
 c = np.load("test_SVD.npy")
-for line in c:
-    #print(line.argsort()[-10:][::-1])
-    for s in line.argsort()[-3:][::-1]:
+# read the mask image 
+bg = imread('/shared/s2/users/jwang/photo/P1.jpg')
+wc = WordCloud(background_color='white',
+        mask= bg,
+        max_font_size=40,
+        random_state=42)
 
-        print(DICT[s],'\t',line[s])
-    print ()
+image_colors = ImageColorGenerator(bg)
+
+k = 0
+prefix = 'frequency'
+for line in c:
+    for s in line.argsort()[-20:][::-1]:
+        name = prefix + str(k)
+        name = {}    
+        name[DICT[s]] = line[s]
+        wc.generate_from_frequencies(name)
+        plt.imshow(wc)
+        plt.axis("off")
+        wc.to_file("haha.png")
+    k += 1
+'''
+
+plt.imshow(wc)
+plt.axis("off")
+
+plt.figure()
+'''
